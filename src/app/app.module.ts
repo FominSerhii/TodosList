52caf2567aed06089885ba02cc3adda4fb7d5eb3
FominSerhii <fominsergeyo15@gmail.com>
2019-03-20 19:12:55 +0200

    add todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule , MatRadioModule, MatInputModule, MatCardModule } from '@angular/material';
 
-import { INITIAL_STATE } from './store/store.models';
-import { reducers } from './store/reducers.models';
 import { AppComponent } from './app.component';
+import { reducers } from './store/reducers.models';
+import { INITIAL_STATE } from './store/store.models';
 import { NewTodoComponent } from './todos/components/new-todo/new-todo.component';
 import { TodoListComponent } from './todos/components/todo-list/todo-list.component';
 import { TodoListItemComponent } from './todos/components/todo-list-item/todo-list-item.component';
@@ -20,13 +20,13 @@ import { TodoListItemComponent } from './todos/components/todo-list-item/todo-li
     TodoListItemComponent
   ],
   imports: [
-    BrowserModule,
     FormsModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
+    BrowserModule,
+    MatCardModule,
     MatRadioModule,
     MatInputModule,
-    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot(reducers, {initialState: INITIAL_STATE})
   ],
   providers: [],
